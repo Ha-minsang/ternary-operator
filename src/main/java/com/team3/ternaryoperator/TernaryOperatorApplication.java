1e115e541f9830f27e2c344c05fb176cffd9ed79
Ha-minsang <gkalstkd@gmail.com>
2025-12-07 14:50:40 +0900

    chore: 기본 구조 설정

---
+++ src/main/java/com/team3/ternaryoperator/TernaryOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.team3.ternaryoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TernaryOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TernaryOperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team3/ternaryoperator/TernaryOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.ternaryoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TernaryOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
