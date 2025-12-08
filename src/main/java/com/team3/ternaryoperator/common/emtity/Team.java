37e472cf5e5efee6e8aad5e40a7382af33cb63a4
Ha-minsang <gkalstkd@gmail.com>
2025-12-08 15:44:47 +0900

    feat: 엔티티 추가

---
+++ src/main/java/com/team3/ternaryoperator/common/emtity/Team.java
@@ -0,0 +1,4 @@
+package com.team3.ternaryoperator.common.emtity;
+
+public class Team extends BaseEntity {
+}
---
+++ src/main/java/com/team3/ternaryoperator/common/emtity/User.java
@@ -0,0 +1,5 @@
+package com.team3.ternaryoperator.common.emtity;
+
+public class User extends BaseEntity {
+
+}
