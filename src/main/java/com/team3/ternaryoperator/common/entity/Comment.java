69478b3abcd026cf66de3871e53326dd0a4af582
sangmu <dltkdandl@naver.com>
2025-12-08 16:38:21 +0900

    feat : comment entity 생성

--- src/main/java/com/team3/ternaryoperator/common/entity/Comment.java
+++ src/main/java/com/team3/ternaryoperator/common/entity/Comment.java
@@ -1,4 +1,43 @@
 package com.team3.ternaryoperator.common.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "parent_id", nullable = false)
+    private Comment parentComment;
+
+    public Comment(String content, User user, Task task, Comment parentComment) {
+        this.content = content;
+        this.user = user;
+        this.task = task;
+        this.parentComment = parentComment;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
