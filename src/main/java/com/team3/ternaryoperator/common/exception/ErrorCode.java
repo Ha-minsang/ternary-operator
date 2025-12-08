c6c6c734729cb00a6288b1ac06f78b4ac094f42d
oezy-coder <a8609556@gmail.com>
2025-12-09 00:12:10 +0900

    chore: 에러코드 TEAM_NAME_DUPLICATED 추가

--- src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
+++ src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "작업을 찾을 수 없습니다."),
 
     // Team 관련 ErrorCode
+    TEAM_NAME_DUPLICATED(HttpStatus.UNAUTHORIZED, "이미 존재하는 팀 이름입니다.");
 
     // Comment 관련 ErrorCode
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.");
