1638c2b28f3649871cb86f3e5e540d6375854268
sangmu <dltkdandl@naver.com>
2025-12-08 17:52:40 +0900

    feat : 작업, 유저, 댓글 404 에러코드 추가

--- src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
+++ src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
@@ -17,7 +17,12 @@ public enum ErrorCode {
     INVALID_TOKEN (HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
 
     // Common 공통 ErrorCode
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다: ");
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다: "),
+
+    // ===== Task / User / Comment 관련 =====
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "작업을 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
