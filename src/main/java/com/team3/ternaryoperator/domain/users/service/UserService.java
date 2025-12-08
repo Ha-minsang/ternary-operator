c6f873dc8a849a87bcc92f806a7713561d2a17b8
Ha-minsang <gkalstkd@gmail.com>
2025-12-08 21:27:02 +0900

    feat: 유저 생성 기능 구현

--- src/main/java/com/team3/ternaryoperator/domain/users/service/UserService.java
+++ src/main/java/com/team3/ternaryoperator/domain/users/service/UserService.java
@@ -1,5 +1,48 @@
 package com.team3.ternaryoperator.domain.users.service;
 
+import com.team3.ternaryoperator.common.entity.User;
+import com.team3.ternaryoperator.common.exception.CustomException;
+import com.team3.ternaryoperator.common.exception.ErrorCode;
+import com.team3.ternaryoperator.domain.users.dto.request.UserCreateRequest;
+import com.team3.ternaryoperator.domain.users.dto.response.UserCreateResponse;
+import com.team3.ternaryoperator.domain.users.enums.UserRole;
+import com.team3.ternaryoperator.domain.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
-    
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserCreateResponse signUp(UserCreateRequest request) {
+
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new CustomException(ErrorCode.DUPLICATE_USERNAME);
+        }
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new CustomException(ErrorCode.DUPLICATE_EMAIL);
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        User user = new User(
+                request.getUsername(),
+                request.getEmail(),
+                request.getName(),
+                encodedPassword,
+                UserRole.USER,
+                null
+        );
+
+        User saved = userRepository.save(user);
+
+        return UserCreateResponse.from(saved);
+    }
 }
