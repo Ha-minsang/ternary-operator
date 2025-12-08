7b01e4e933c8f3cfbd21d14c9939d7f504075d3e
Ha-minsang <gkalstkd@gmail.com>
2025-12-08 10:35:21 +0900

    refactor: api 명세서에 맞는 형식의 응답으로 리팩토링

--- src/main/java/com/team3/ternaryoperator/common/dto/CommonResponse.java
+++ src/main/java/com/team3/ternaryoperator/common/dto/CommonResponse.java
@@ -9,43 +9,34 @@ import java.time.LocalDateTime;
 public class CommonResponse<T> {
 
     private final boolean success;         // 성공 여부
-    private final String code;             // 성공시 SUCCESS , 실패시 CustomErrorCode
     private final String message;          // 성공시 요청이 성공했습니다. , 실패시 error message
-    private final String path;             // 실패 시만 사용
-    private final LocalDateTime timestamp; // 실패 시만 사용
-    private final T data;                  // 성공 시만 사용
+    private final LocalDateTime timestamp; // 실행 시간
+    private final T data;                  // 성공 시 data , 실패시 null
 
-    private CommonResponse(boolean success, String code, String message,
-                           String path, LocalDateTime timestamp, T data) {
+    private CommonResponse(boolean success, String message, T data, LocalDateTime timestamp ) {
         this.success = success;
-        this.code = code;
         this.message = message;
-        this.path = path;
-        this.timestamp = timestamp;
         this.data = data;
+        this.timestamp = timestamp;
     }
 
     // 성공시 공용 응답 객체
     public static <T> CommonResponse<T> success(T data) {
-        return new CommonResponse<>(true, "SUCCESS", "요청이 성공했습니다.",
-                null, null, data);
+        return new CommonResponse<>(true, "요청이 성공했습니다.", data, LocalDateTime.now());
     }
 
     // 실패시 공용 응답 객체
-    public static <T> CommonResponse<T> fail(ErrorCode errorCode, String path) {
-        return new CommonResponse<>(false, errorCode.getCode(), errorCode.getMessage(),
-                path, LocalDateTime.now(), null);
+    public static <T> CommonResponse<T> fail(ErrorCode errorCode) {
+        return new CommonResponse<>(false, errorCode.getMessage(),null, LocalDateTime.now());
     }
 
     // 메세지를 직접 입력하는 경우
-    public static <T> CommonResponse<T> fail(ErrorCode errorCode, String message, String path) {
+    public static <T> CommonResponse<T> fail(ErrorCode errorCode, String message) {
         return new CommonResponse<>(
                 false,
-                errorCode.getCode(),
                 message,
-                path,
-                LocalDateTime.now(),
-                null
+                null,
+                LocalDateTime.now()
         );
     }
 }
--- src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
+++ src/main/java/com/team3/ternaryoperator/common/exception/ErrorCode.java
@@ -9,15 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // Auth(인증,권한) 관련 ErrorCode
-    AUTH_INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_001", "아이디 또는 비밀번호가 올바르지 않습니다."),
-    NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "AUTH_002", "로그인이 되어 있지 않습니다."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "AUTH_003", "해당 작업을 수행할 권한이 없습니다."),
-    ALREADY_LOGGED_IN(HttpStatus.CONFLICT, "AUTH_004", "이미 로그인이 되어 있습니다."),
+    AUTH_INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
+    NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "로그인이 되어 있지 않습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 작업을 수행할 권한이 없습니다."),
+    ALREADY_LOGGED_IN(HttpStatus.CONFLICT, "이미 로그인이 되어 있습니다."),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
+    INVALID_TOKEN (HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
 
     // Common 공통 ErrorCode
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "COMMON_001", "요청 값이 올바르지 않습니다: ");
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다: ");
 
     private final HttpStatus status;
-    private final String code;
     private final String message;
 }
