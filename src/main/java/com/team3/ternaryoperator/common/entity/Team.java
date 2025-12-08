06e27e1102dbad8ba461ac1e845de8bd4ce6d010
sangmu <dltkdandl@naver.com>
2025-12-08 17:52:53 +0900

    feat : Task, User, Team 임의 코드 생성

--- src/main/java/com/team3/ternaryoperator/common/entity/Team.java
+++ src/main/java/com/team3/ternaryoperator/common/entity/Team.java
@@ -1,4 +1,17 @@
 package com.team3.ternaryoperator.common.entity;
 
+import com.team3.ternaryoperator.domain.users.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "teams")
 public class Team extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
