75b65a573b0c32aa52573db92c83b74da307fe01
Ha-minsang <gkalstkd@gmail.com>
2025-12-09 14:18:43 +0900

    refactor: Json 포맷 삭제

--- src/main/java/com/team3/ternaryoperator/common/entity/BaseEntity.java
+++ src/main/java/com/team3/ternaryoperator/common/entity/BaseEntity.java
@@ -1,6 +1,5 @@
 package com.team3.ternaryoperator.common.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -18,15 +17,9 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
-            timezone = "UTC")
     private LocalDateTime createdAt;    // 생성일
 
     @LastModifiedDate
-    @JsonFormat(shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
-            timezone = "UTC")
     private LocalDateTime updatedAt;   // 수정일
 
     private LocalDateTime deletedAt;
