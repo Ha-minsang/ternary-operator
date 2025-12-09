f7ed234462de2b686d8d0c7ee9e5e9b16ca8be19
Ha-minsang <gkalstkd@gmail.com>
2025-12-09 20:54:16 +0900

    fix: 레포지토리 쿼리문이 없어 발생하는 버그 수정

--- src/main/java/com/team3/ternaryoperator/domain/user/repository/UserRepository.java
+++ src/main/java/com/team3/ternaryoperator/domain/user/repository/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     List<User> findByTeamIdNot(Long teamId);
+
+    List<User> findByTeamId(Long teamId);
 }
