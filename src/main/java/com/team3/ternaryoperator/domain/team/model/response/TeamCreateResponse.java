47c8a697d8573ee047a6d69d91a3448a9dc55cae
oezy-coder <a8609556@gmail.com>
2025-12-09 15:26:25 +0900

    refactor: Team API 위치 및 코드 구조 정리 - Team API를 domain.team 패키지로 이동 - TeamCreateResponse에서 사용하지 않는 import 제거

---
+++ src/main/java/com/team3/ternaryoperator/domain/team/model/response/TeamCreateResponse.java
@@ -0,0 +1,36 @@
+package com.team3.ternaryoperator.domain.team.model.response;
+
+import com.team3.ternaryoperator.domain.team.model.dto.MemberDto;
+import com.team3.ternaryoperator.domain.team.model.dto.TeamDto;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class TeamCreateResponse {
+
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final LocalDateTime createdAt;
+    private final List<MemberDto> members;
+
+    public TeamCreateResponse(Long id, String name, String description, LocalDateTime createdAt, List<MemberDto> members) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.members = members;
+    }
+
+    public static TeamCreateResponse from(TeamDto dto, List<MemberDto> members) {
+        return new TeamCreateResponse(
+                dto.getId(),
+                dto.getName(),
+                dto.getDescription(),
+                dto.getCreatedAt(),
+                members
+        );
+    }
+}
